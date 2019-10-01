93155f802a8a1e7dbc779e2cddcb8bc4d16d6845
Sharvil Kekre <sharvilkekre@gmail.com>
2019-10-01 19:48:14 -0400

    added pandas dataframe

--- Query/views.py
+++ Query/views.py
@@ -1,5 +1,12 @@
+import os
+import pandas as pd
 from django.shortcuts import render
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 def index(request):
+    df = pd.read_csv(os.path.join(BASE_DIR, 'Query/data/video_game.csv'))
+    for index, row in df.iterrows():
+        pass
     return render(request, 'index.html')
