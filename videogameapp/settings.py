1d240d6eb1a38a87b237a61623b78f33d86ced08
Sharvil Kekre <sharvilkekre@gmail.com>
2019-10-11 12:05:19 -0400

    added boostrap files

--- videogameapp/settings.py
+++ videogameapp/settings.py
@@ -120,4 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = (
+
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'Query/static'),
+)
+
 STATIC_URL = '/static/'
